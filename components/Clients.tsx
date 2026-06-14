8e403a2bdbf42787c2f6eb9ad31efaf6b60a5a9e
Avinesh Harikrishnan <avineshwnp@gmail.com>
2026-06-14 21:23:08 +0530

    push check

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -18,7 +18,7 @@ export default function Clients() {
             >
               {client.icon && (
                 <div className="w-5 h-5 flex items-center justify-center shrink-0">
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  {/* eslint-disable-next-line @next/next/no-img-element -*/}
                   <img
                     src={isLocal ? client.icon : `https://cdn.simpleicons.org/${client.icon}`}
                     alt={client.logo}
